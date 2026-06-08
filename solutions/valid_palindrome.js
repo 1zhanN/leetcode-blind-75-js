62dae84bea0ed51a62375efe7cbd0df86b517f56
Izhan Nadeem <57583461+1zhanN@users.noreply.github.com>
2026-06-08 03:30:20 +0200

    Refactor isPalindrome function and remove comments

--- solutions/valid_palindrome.js
+++ solutions/valid_palindrome.js
@@ -1,35 +1,3 @@
-/*
-// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
-function twoSum(nums, target){
-    output = []
-    for (let i = 0; i < nums.length; i++){
-        for (let j = i+1; j < nums.length; j++){
-            if(nums[i] + nums[j]==target){
-                output.push(i)
-                output.push(j)
-                return output
-            }
-        }
-    }
-}
-*/
-/*
-//Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
-
-function missingNumber(nums){
-    sorted = nums.sort((a, b) => a - b)
-    for (let i = 0; i < sorted.length; i++){
-        if (sorted[i] != i){
-            return i
-        }  
-    return sorted.length 
-} 
-}
-
-*/
-
-
-
 /*A phrase is a palindrome if, after converting all uppercase letters into lowercase letters 
 and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include 
 letters and numbers. */
