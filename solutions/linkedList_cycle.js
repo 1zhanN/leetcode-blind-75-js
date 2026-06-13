c732cc6727c5619554be7ac5d6ff933726203544
Izhan Nadeem <izhann000@gmail.com>
2026-06-13 14:20:36 +0200

    added core logic for finding cycles in a linked list

--- solutions/linkedList_cycle.js
+++ solutions/linkedList_cycle.js
@@ -1,3 +1,35 @@
+// leetcode 141. Linked List Cycle
+/*
+They are essentially saying: "We built a loop using a secret number. 
+We aren't going to tell you what that number is, or if we even used one. 
+By looking only at the nodes themselves, can you figure out if we made a loop?"*/
+
+function ListNode(val) {
+    this.val = val;
+    this.next = null;
+}
+head = new ListNode(3);
+node2 = head.next = new ListNode(2);
+node3 = node2.next = new ListNode(0);
+node4 = node3.next = new ListNode(-4);
+node4.next = head;
+
 var hasCycle = function(head) {
-    
+    if (head == null || head.val == null || head.next == null) {
+        return false;
+    }
+    let slow = head;
+    let fast = head;
+while (fast != null && fast.next != null) {
+
+    slow = slow.next;
+    fast = fast.next.next;
+
+        if (slow === fast) {
+        return true;
+    }
+}   return false;
+
 };
+
+console.log(hasCycle(head)); // true
